1f78c2c7886949ce1179e54597a3bfdc312b8595
snosenzo <samnosenzo@gmail.com>
2025-08-26 11:16:35 +0200

    vi nav

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -53,7 +53,7 @@ U_NP,              U_NP,              &kp K_APP,         &kp SPACE,         &kp 
 
 
 // VI like nav layer with changed thumb cluster
-#define MIRYOKU_ALTERNATIVES_NAV \
+#define MIRYOKU_ALTERNATIVES_NAV_VI \
 U_BOOT,            &u_to_U_TAP,       &u_to_U_EXTRA,     &u_to_U_BASE,      U_NA,              U_RDO,             U_PST,             U_CPY,             U_CUT,             U_UND,             \
 &kp LGUI,          &kp LALT,          &kp LCTRL,         &kp LSHFT,         U_NA,              &kp LEFT,          &kp DOWN,          &kp UP,            &kp RIGHT,         &u_caps_word,      \
 U_NA,              &kp RALT,          &u_to_U_NUM,       &u_to_U_NAV,       U_NA,              &kp INS,           &kp HOME,          &kp PG_DN,         &kp PG_UP,         &kp END,           \
