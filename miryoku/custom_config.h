4db128a5449f00f9f31084016a655a3c11c46251
snosenzo <samnosenzo@gmail.com>
2025-08-26 10:29:06 +0200

    maybe fix

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -98,10 +98,10 @@ U_NP,              U_NP,              &to U_BASE,        &none,             &kp 
      K20, K21, K22, K23, K24,      K25, K26, K27, K28, K29, \
      N30, N31, K32, K33, K34,      K35, K36, K37, N38, N39 \
 ) \
-&kp_TAB   K00  K01  K02  K03  K04       K05  K06  K07  K08  K09  &u_to_U_BASE \
-&kp_LSHFT K10  K11  K12  K13  K14       K15  K16  K17  K18  K19  XXX \
-&kp_LCTRL K20  K21  K22  K23  K24       K25  K26  K27  K28  K29  XXX \
-                    K32  K33  K34       K35  K36  K37
+TAB   K00  K01  K02  K03  K04       K05  K06  K07  K08  K09  &u_to_U_BASE \
+LSHFT K10  K11  K12  K13  K14       K15  K16  K17  K18  K19  XXX \
+LCTRL K20  K21  K22  K23  K24       K25  K26  K27  K28  K29  XXX \
+                K32  K33  K34       K35  K36  K37
 // end this
 
 #define MIRYOKU_LAYERMAPPING_GAMENUM MIRYOKU_MAPPING
